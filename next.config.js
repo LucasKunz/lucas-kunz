bb2a5270cec405def14aea4c909fbbbfdc078aa7
Lucas Kunz <lucaskunz1@gmail.com>
2022-05-17 22:24:54 -0300

    Adjustment build

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
 module.exports = {
   compiler: {
     // ssr and displayName are configured by default
     styledComponents: true,
   },
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 }
 
-module.exports = nextConfig
