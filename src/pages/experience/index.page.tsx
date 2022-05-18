4a963d577b647482fe0336b9e617b5629683b946
Lucas Kunz <lucaskunz1@gmail.com>
2022-05-17 22:24:35 -0300

    Adjustment build

--- src/pages/experience/index.page.tsx
+++ src/pages/experience/index.page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Main, Text, Title } from "./style";
+import { Main, Text, Title } from "./styled";
 
 
 const Experiences = () => {
